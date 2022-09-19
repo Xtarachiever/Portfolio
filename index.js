b6d03c7634b639221dea31874466f2996b9ca5d4
Xtarachiever <estheradeyemi796@gmail.com>
2022-09-19 11:23:32 +0100

    Updated portfolio

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const navBar = document.querySelector('#navbar');
-const navContent = document.querySelector('#nav-content')
+const navContent = document.querySelector('#nav-content');
+const sections = document.querySelector('.sections');
 
 navBar.addEventListener('click', ()=>{
     if(navContent.className === "nav-content"){
-        navContent.className += " hidden"
+        navContent.className += " hidden";
+        sections.className += 'sections';
     }
     else{
-        navContent.className = "nav-content"
+        navContent.className = "nav-content";
+        sections.className += ' small-screen';
     }
 })
